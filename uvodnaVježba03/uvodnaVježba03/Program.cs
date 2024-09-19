c81cb2681d9a3ed504f4f54d2960553d3752fabc
DavidCuturic5 <david.cuturic5@gmail.com>
2024-09-19 17:55:05 +0200

    Add files via upload

---
+++ uvodnaVježba03/uvodnaVježba03/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVježba03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Unesite vrijednost stranice x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Unesite vrijednost stranice y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Unesite vrijednost stranice z:");
+            int z = Convert.ToInt32(Console.ReadLine());
+
+
+            if (Pravokutnitrokut(x, y, z))
+            {
+                Console.WriteLine("Trokut je pravokutan");
+            }
+            else
+            {
+                Console.WriteLine("Trokut nije pravokutan");
+            }
+            Console.ReadKey();
+        }
+        static bool Pravokutnitrokut(int x, int y, int z)
+        {
+            return (x * x + y * y == z * z) || (x * x + z * z == y * y) || (y * y + z * z == x * x);
+        }
+
+        
+
+
+
+
+        
+    }
+}
+    
+
